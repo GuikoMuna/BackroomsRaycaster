1db569400b6a0be4198aa6f384c9fd9fbc4d0bec
Magos-Logis Guiko-20 <dragaocomuna@gmail.com>
2025-03-11 14:10:09 -0300

    Main and Maze

--- main.c
+++ main.c
@@ -1,12 +1,209 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <GL/glut.h>
 #include <math.h>
+#include <time.h>
 
 #define PI 3.1415926535
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+#define ORIGINAL_MAZE_WIDTH 20
+#define ORIGINAL_MAZE_HEIGHT 15
+#define CONVERTED_MAZE_WIDTH (2 * ORIGINAL_MAZE_WIDTH + 1)
+#define CONVERTED_MAZE_HEIGHT (2 * ORIGINAL_MAZE_HEIGHT + 1)
 
-#define WIDTH 800
-#define HEIGHT 600
+// Maze Logic -------------------------------------------------------------
+typedef struct {
+    int x, y;
+} Vector2Int;
+
+typedef struct {
+    int walls; // Bitmask for walls (e.g., 0b1111 for all walls)
+    bool visited;
+} MazeCell;
+
+MazeCell maze[ORIGINAL_MAZE_WIDTH][ORIGINAL_MAZE_HEIGHT];
+bool convertedMaze[CONVERTED_MAZE_WIDTH][CONVERTED_MAZE_HEIGHT]; // Global Maze
+unsigned int rVisits = ORIGINAL_MAZE_WIDTH * ORIGINAL_MAZE_HEIGHT;
+
+typedef struct Vector2IntNode {
+    Vector2Int data;
+    struct Vector2IntNode* next;
+} Vector2IntNode;
+
+Vector2IntNode* mainStack = NULL;
+
+// Stack functions
+Vector2IntNode* push(Vector2IntNode* stack, Vector2Int data) {
+    Vector2IntNode* node = (Vector2IntNode*)malloc(sizeof(Vector2IntNode));
+    if (!node) exit(1);
+    node->data = data;
+    node->next = stack;
+    return node;
+}
+
+Vector2Int pop(Vector2IntNode** stack) {
+    if (*stack == NULL) {
+        return (Vector2Int){-1, -1}; // Invalid value
+    }
+    Vector2IntNode* temp = *stack;
+    Vector2Int data = temp->data;
+    *stack = temp->next;
+    free(temp);
+    return data;
+}
+
+int getStackSize(Vector2IntNode* stack) {
+    int size = 0;
+    while (stack) {
+        size++;
+        stack = stack->next;
+    }
+    return size;
+}
+
+Vector2Int getFromIndex(Vector2IntNode* stack, int index) {
+    for (int i = 0; i < index; i++) {
+        stack = stack->next;
+    }
+    return stack->data;
+}
+
+void removeWalls(Vector2Int a, Vector2Int b) {
+    if (a.x == b.x) {
+        if (a.y < b.y) {
+            maze[a.x][a.y].walls &= ~0b0100; // Clear bottom wall of a
+            maze[b.x][b.y].walls &= ~0b0001; // Clear top wall of b
+        } else {
+            maze[a.x][a.y].walls &= ~0b0001; // Clear top wall of a
+            maze[b.x][b.y].walls &= ~0b0100; // Clear bottom wall of b
+        }
+    } else {
+        if (a.x < b.x) {
+            maze[a.x][a.y].walls &= ~0b0010; // Clear right wall of a
+            maze[b.x][b.y].walls &= ~0b1000; // Clear left wall of b
+        } else {
+            maze[a.x][a.y].walls &= ~0b1000; // Clear left wall of a
+            maze[b.x][b.y].walls &= ~0b0010; // Clear right wall of b
+        }
+    }
+}
+
+Vector2Int checkNbs(Vector2Int curr) {
+    Vector2Int nbs[4] = {
+        {curr.x, curr.y - 1}, // Top
+        {curr.x + 1, curr.y}, // Right
+        {curr.x, curr.y + 1}, // Bottom
+        {curr.x - 1, curr.y}  // Left
+    };
+    Vector2IntNode* stack = NULL;
+
+    for (int i = 0; i < 4; i++) {
+        if (nbs[i].x >= 0 && nbs[i].x < ORIGINAL_MAZE_WIDTH && nbs[i].y >= 0 && nbs[i].y < ORIGINAL_MAZE_HEIGHT) {
+            if (!maze[nbs[i].x][nbs[i].y].visited) {
+                stack = push(stack, nbs[i]);
+            }
+        }
+    }
+
+    int stackSize = getStackSize(stack);
+    if (stackSize > 0) {
+        int index = rand() % stackSize;
+        Vector2Int next = getFromIndex(stack, index);
+        removeWalls(curr, next);
+
+        while (stack) pop(&stack);
+        return next;
+    }
+
+    while (stack) pop(&stack);
+    return (Vector2Int){-1, -1};
+}
+
+void convertMaze() {
+    for (int x = 0; x < CONVERTED_MAZE_WIDTH; x++) {
+        for (int y = 0; y < CONVERTED_MAZE_HEIGHT; y++) {
+            convertedMaze[x][y] = true;
+        }
+    }
+
+    for (int x = 0; x < ORIGINAL_MAZE_WIDTH; x++) {
+        for (int y = 0; y < ORIGINAL_MAZE_HEIGHT; y++) {
+            int cx = 2 * x + 1;
+            int cy = 2 * y + 1;
+            convertedMaze[cx][cy] = false;
+
+            if (!(maze[x][y].walls & 0b0001)) convertedMaze[cx][cy - 1] = false;
+            if (!(maze[x][y].walls & 0b0010)) convertedMaze[cx + 1][cy] = false;
+            if (!(maze[x][y].walls & 0b0100)) convertedMaze[cx][cy + 1] = false;
+            if (!(maze[x][y].walls & 0b1000)) convertedMaze[cx - 1][cy] = false;
+        }
+    }
+}
+
+void generate_maze() {
+    srand(time(NULL));
+
+    for (int x = 0; x < ORIGINAL_MAZE_WIDTH; x++) {
+        for (int y = 0; y < ORIGINAL_MAZE_HEIGHT; y++) {
+            maze[x][y].walls = 0b1111;
+            maze[x][y].visited = false;
+        }
+    }
+
+    Vector2Int curr = {0, 0};
+    mainStack = push(mainStack, curr);
+    maze[curr.x][curr.y].visited = true;
+    rVisits--;
+
+    while (rVisits > 0) {
+        Vector2Int next = checkNbs(curr);
+        if (next.x != -1) {
+            mainStack = push(mainStack, next);
+            maze[next.x][next.y].visited = true;
+            rVisits--;
+            curr = next;
+        } else {
+            curr = pop(&mainStack);
+        }
+    }
+
+    convertMaze();
+    while (mainStack) pop(&mainStack);
+}
+
+void drawMaze() {
+    // Get the screen dimensions
+    int screenWidth = glutGet(GLUT_WINDOW_WIDTH);
+    int screenHeight = glutGet(GLUT_WINDOW_HEIGHT);
+
+    // Calculate the size of each square based on the screen size and maze dimensions
+    float squareWidth = (float)screenWidth / CONVERTED_MAZE_WIDTH;
+    float squareHeight = (float)screenHeight / CONVERTED_MAZE_HEIGHT;
+
+    // Loop through the maze matrix
+    for (int i = 0; i < CONVERTED_MAZE_HEIGHT; i++) {
+        for (int j = 0; j < CONVERTED_MAZE_WIDTH; j++) {
+            // Set the color based on the value in the matrix
+            if (convertedMaze[j][i]) {
+                glColor3f(1.0f, 1.0f, 1.0f); // White (wall)
+            } else {
+                glColor3f(0.0f, 0.0f, 0.0f); // Black (open space)
+            }
+
+            // Draw the square
+            glBegin(GL_QUADS);
+            glVertex2f(j * squareWidth, i * squareHeight);
+            glVertex2f((j + 1) * squareWidth, i * squareHeight);
+            glVertex2f((j + 1) * squareWidth, (i + 1) * squareHeight);
+            glVertex2f(j * squareWidth, (i + 1) * squareHeight);
+            glEnd();
+        }
+    }
+}
+
+// Player Logic -------------------------------------------------------------
 
 struct Player
 {
@@ -38,7 +235,8 @@ void drawPlayer()
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    drawPlayer();
+    drawMaze(); // Draw the maze
+    drawPlayer(); // Draw the player
     glutSwapBuffers();
 }
 
@@ -74,18 +272,19 @@ void buttons(unsigned char key, int x, int y)
 void initBeforeRender()
 {
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    gluOrtho2D(0, WIDTH, HEIGHT, 0);
+    gluOrtho2D(0, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
 }
 
 int main(int argc, char** argv)
-{ 
+{
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-    glutCreateWindow("Backrooms");
-    glutInitWindowSize(WIDTH, HEIGHT);
+    glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    glutCreateWindow("Raycaster");
 
     initBeforeRender();
-    
+    generate_maze(); // Generate the maze
+
     glutDisplayFunc(display);
     glutKeyboardFunc(buttons);
     glutMainLoop();
